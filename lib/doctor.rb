bb50cd23b36ae15c4ebbe54497dc3385772aa4ca
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-08 13:33:48 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -6,6 +6,10 @@ class Doctor
     @@all << self
   end;
 
+  def new_appointment(patient, date)
+    appointment = Appointment.new(date, patient, self)
+  end
+
   def appointments
     Appointment.all.select{|appt| appt.doctor == self}
   end
