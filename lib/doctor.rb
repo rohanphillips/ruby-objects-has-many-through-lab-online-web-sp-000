5dad07bbd133f963d3a99fd42def29b59ad160b2
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-08 13:39:20 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -10,6 +10,14 @@ class Doctor
     appointment = Appointment.new(date, patient, self)
   end
 
+  def patients
+      collection = []
+      Appointment.all.select{|appt| appt.doctor == self}.each do |appt|
+        collection << appt.patient
+      end
+      collection
+  end
+
   def appointments
     Appointment.all.select{|appt| appt.doctor == self}
   end
