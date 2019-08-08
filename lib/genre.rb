b0e05a06ede95dc8545cd489644a68d1397fe744
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-08 02:17:02 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,20 @@
+class Genre 
+  @@all = []
+
+  def initialize(genre_name)
+    @genre = genre_name
+    @@all << self
+  end
+
+  def songs
+
+  end
+
+  def artists
+
+  end
+
+  def self.all
+    @@all
+  end
+end
